6d69ebc83f548d6a33b4fcef89a5390963c21d16
Armann David Sigurdsson <armannds@gmail.com>
2018-08-31 16:25:59 +0200

    Some stuff....

--- src/main/java/com/armannds/hikariboot/repository/UserRepository.java
+++ src/main/java/com/armannds/hikariboot/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package com.armannds.hikariboot.repository;
 
-import com.armannds.hikariboot.entity.UserEntity;
+import com.armannds.hikariboot.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 
 }
